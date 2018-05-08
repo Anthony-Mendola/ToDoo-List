9cf0e225232a8530ab2f16e2eb136073f4a9eeb7
Anthony <github email address>
2018-05-07 22:04:14 -0400

    add todo feature completed

--- app.js
+++ app.js
@@ -26,6 +26,22 @@ function addTask(e) {
   li.className = 'collection-item';
   //Create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  //Add class
+  link.className = 'delete-item secondary-content';
+  //Icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  //Append the link to li
+  li.appendChild(link);
+
+  //Append li to ul
+  taskList.appendChild(li);
+
+  //Clear input
+  taskInput.value = '';
+
+
 
   e.preventDefault();
 }
