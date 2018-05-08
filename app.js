447ab55ca950f1b79cc3aae184a096671236c213
Anthony <github email address>
2018-05-07 22:40:08 -0400

    added filter feature

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ function loadEventListeners() {
   taskList.addEventListener('click', removeTask);
   //Clear task event
   clearBtn.addEventListener('click', clearTasks);
+  //Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 //Add task
@@ -68,4 +70,18 @@ function clearTasks() {
     taskList.removeChild(taskList.firstChild);
   }
 
+}
+
+//Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
